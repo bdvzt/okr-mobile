49260bcc02883992b7051d751c24f0bbfdaa1e0c
bdvzt <budaevazay@mail.ru>
2025-02-24 22:57:35 +0700

    Initial commit

---
+++ OKR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OKR
+//
+//  Created by Zayata Budaeva on 24.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
